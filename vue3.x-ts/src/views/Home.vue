7937150989e69a771111fb44b4fb8b7777c55fd9
Ti2000 <1561121084@qq.com>
2021-05-24 17:53:38 +0800

    四次提交

---
+++ vue3.x-ts/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, defineProps, defineEmit, useContext } from 'vue'
+import HelloWorld from '../components/HelloWorld.vue'
+
+onMounted(() => {
+  console.log('组件加载完毕')
+})
+</script>
